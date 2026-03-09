7faa55b5b6e6028bb293b34da8d7013855240f55
CarlosToledo110 <carlostoledo1250@gmail.com>
2026-03-09 14:48:17 -0600

    Funcionalidad del Menú Principal del sistema

--- Proyecto01CarlosToledo110/Program.cs
+++ Proyecto01CarlosToledo110/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿int opcionmenuprincipal;
+Console.WriteLine("Bienvenido al sistema de la plataforma de streaming, elija una opción");
+Console.WriteLine("1. Evaluar un nuevo contenido");
+Console.WriteLine("2. Mostrar reglas del sistema");
+Console.WriteLine("3. Mostrar estadísticas de la sesión");
+Console.WriteLine("4. Reiniciar estadísticas");
+Console.WriteLine("5. Salir");
+
+opcionmenuprincipal = int.Parse(Console.ReadLine());
+
+switch (opcionmenuprincipal)
+{
+    case 1:
+        {
+            break;
+        }
+    case 2:
+        {
+            break;
+        }
+    case 3:
+        {
+            break;
+        }
+    case 4:
+        {
+            break;
+        }
+    case 5:
+        {
+            break;
+        }
+    default:
+        {
+            break;
+        }
+}
